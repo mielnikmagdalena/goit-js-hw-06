00a5cf567cba378cc50105004f79aaec60aaddd3
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-06-01 23:05:34 +0200

    The latest changes in Homework 6

--- js/task-02.js
+++ js/task-02.js
@@ -9,18 +9,20 @@ const ingredients = [
 
 // Pobieram listę ul.ingredients
 const ingredientsList = document.querySelector("#ingredients");
-
+//Tworzę zmienną z pustą tablicą, do której będę 'pushować' elementy 'li'
+const liElements = [];
 // Iteruję przez każdy element tablicy ingredients
 ingredients.forEach((ingredient) => {
   // Tworzę nowy element <li>
-  const listItem = document.createElement("li");
+  const li = document.createElement("li");
 
   // Dodaję zawartość tekstową (nazwę składnika) do elementu <li>
-  listItem.textContent = ingredient;
+  li.textContent = ingredient;
 
   // Dodaję klasę 'item' do elementu <li>
-  listItem.classList.add("item");
-
-  // Dodaję element <li> do listy ul#ingredients
-  ingredientsList.appendChild(listItem);
+  li.classList.add("item");
+  // 'Pushuję' elementy 'li' do zmiennej liElements, która jest pustą tablicą;
+  liElements.push(li);
 });
+// Dodaję element <li> do listy ul#ingredients za pomocą metody .append
+ingredientsList.append(...liElements);
--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,11 @@
 //Pobieram referencję do pola input o id "validation-input" za pomocą metody querySelector() i przypisuje go do zmiennej validationInput.
 const validationInput = document.querySelector("#validation-input");
-const dataLength = validationInput.getAttribute("data-length");
+
 //Dodaję nasłuchiwanie zdarzenia "blur" do pola input validationInput za pomocą metody addEventListener().
-//Wewnątrz funkcji obsługi zdarzenia, sprawdzam, czy wartość pola input jest równe długości 6 symbolom (validationInput.value.length === 6).
+//Wewnątrz funkcji obsługi zdarzenia, sprawdzam, czy wartość pola input jest równa długości 6 symbolom oraz konwertuję za pomocą metody Number() string na liczbę.
 //Jeśli tak, dodaję klasę "valid", w przeciwnym razie dodaję klasę "invalid".
 validationInput.addEventListener("blur", () => {
-  if (validationInput.value.length === dataLength) {
+  if (validationInput.value.length === Number("6")) {
     validationInput.classList.add("valid");
     validationInput.classList.remove("invalid");
   } else {
