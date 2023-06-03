f087c65832e5683a9cecde43cfdac5a8d577b104
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-06-03 15:17:52 +0200

    the amendment of task 6 Homework 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,13 @@
 //Pobieram referencję do pola input o id "validation-input" za pomocą metody querySelector() i przypisuje go do zmiennej validationInput.
 const validationInput = document.querySelector("#validation-input");
-
+//Ponieważ do pobrania wartości właściwości data-atrybutu używana jest właściwość dataset, po której znajduje się nazwa atrybutu, wykorzystuję ją tutaj, aby pobrać wartość liczbową i przypisuję do zmiennej 'dataLength'
+const dataLength = validationInput.dataset.length;
 //Dodaję nasłuchiwanie zdarzenia "blur" do pola input validationInput za pomocą metody addEventListener().
-//Wewnątrz funkcji obsługi zdarzenia, sprawdzam, czy wartość pola input jest równa długości 6 symbolom oraz konwertuję za pomocą metody Number() string na liczbę.
-//Jeśli tak, dodaję klasę "valid", w przeciwnym razie dodaję klasę "invalid".
+//Sprawdzam warunek, czy długość wartości wprowadzonej przez użytkownika (validationInput.value.length) jest równa wartości dataLength
+// Metoda : parseInt(dataLength) służy do konwersji wartości dataLength na liczbę, ponieważ atrybuty data są zawsze przechowywane jako ciągi znaków (stringi).
+// Jeśli warunek jest spełniony, czyli długość wprowadzonej wartości jest taka sama jak oczekiwana długość, wykonujemy instrukcje w bloku if czyli dodaję klasę "valid", w przeciwnym razie dodaję klasę "invalid".
 validationInput.addEventListener("blur", () => {
-  if (validationInput.value.length === Number("6")) {
+  if (validationInput.value.length === parseInt(dataLength)) {
     validationInput.classList.add("valid");
     validationInput.classList.remove("invalid");
   } else {
